019634f954a67b4c189bfc79466b6177d00dbb08
Sahib Singh <71924657+SahibSingh1@users.noreply.github.com>
2022-03-12 16:52:17 +0530

    Update stress_prediction.py

--- stress_prediction.py
+++ stress_prediction.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[24]:
 
 
 import numpy 
@@ -10,46 +7,8 @@ from sklearn import linear_model
 
 df=pd.read_csv('SaYoPillow.csv')
 
-
-# In[25]:
-
-
-df.isnull().sum()
-
-
-# In[26]:
-
-
-df.shape
-
-
-# In[27]:
-
-
-df
-
-
-# In[28]:
-
-
 reg=linear_model.LinearRegression()
-
-
-# In[29]:
-
-
 reg.fit(df[['sr','rr','t','lm','bo','rem','sr.1','hr']],df.sl)
-
-
-# In[30]:
-
-
-reg.coef_
-
-
-# In[41]:
-
-
 
 sr=float(input("Enter snoring rate"))
 rr=float(input("Enter respiration rate"))
@@ -59,16 +18,10 @@ bo=float(input("Enter blood oxygen"))
 rem=float(input("Enter eye movement"))
 sr1=float(input("Enter sleeping hours"))
 hr=float(input("Enter heart rate"))
+
 print("Your Stress Rate is",int(reg.predict([[sr,rr,t,lm,bo,rem,sr199,hr]])))
 
 
-# In[40]:
-
-
-int("1.8")
-
-
-# In[ ]:
 
 
 
